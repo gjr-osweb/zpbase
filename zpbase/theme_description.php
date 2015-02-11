fea8254eb2e65722efcc10b3577f278db7455ea5
acrylian <wartgart@gmx.net>
2015-02-11 10:09:33 +0100

    Version does not match GitHub version.

--- zpbase/theme_description.php
+++ zpbase/theme_description.php
@@ -4,7 +4,7 @@
 // http://www.oswebcreations.com
 $theme_description['name'] = 'zpBase';
 $theme_description['author'] = 'gjr - oswebcreations.com';
-$theme_description['version'] = '0.97';
+$theme_description['version'] = '1.0';
 $theme_description['date'] = '4/28/14';
 $theme_description['desc'] = 'A Zenphoto/Zenpage loaded "base" starter theme, responsive, ready for custom styling or great as is.';
 ?>
